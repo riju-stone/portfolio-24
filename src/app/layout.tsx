991c468bff0a744e8f2ee79f723ebc33bad0d4b5
Arighna Chakraborty <riju23chakra@gmail.com>
2024-10-20 12:30:12 +0530

    added: header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,27 @@
 import type { Metadata } from "next";
 import "./globals.scss";
 import BackgroundComponent from "@/components/background/Background";
+import HeaderComponent from "@/components/header/Header";
 
 export const metadata: Metadata = {
-	title: "Portfolio-24 Arighna",
-	description: "Arighna Chakraborty Portfolio",
+  title: "Portfolio-24 Arighna",
+  description: "Arighna Chakraborty Portfolio",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en'>
-			<body className='app'>
-				<main>{children}</main>
-				<BackgroundComponent />
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className="app">
+        <main>
+          <HeaderComponent />
+          {children}
+          <BackgroundComponent />
+        </main>
+      </body>
+    </html>
+  );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+
+import styles from "./styles.module.scss";
+import Link from "next/link";
 
 function HeaderComponent() {
   return (
-    <div>HeaderComponent</div>
-  )
+    <div className={styles.headerWrapper}>
+      <div className={styles.initialsContainer}>A - C</div>
+      <div className={styles.linksContainer}>
+        <Link href="/">home</Link>
+        <Link href="/blog">blog</Link>
+        <Link href="/shelf">shelf</Link>
+      </div>
+      <div className={styles.themeSwitchContainer}>O</div>
+    </div>
+  );
 }
 
-export default HeaderComponent
+export default HeaderComponent;
+
