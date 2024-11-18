e443e1b2333ef8ad4cfd6a753de24c01960cb006
Arighna Chakraborty <riju23chakra@gmail.com>
2024-11-18 23:13:02 +0530

    fixed browser api errors

--- src/components/hamburger/Hamburger.tsx
+++ src/components/hamburger/Hamburger.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { space_grotesk } from "@/utils/fonts";
-import { hamburgerMenuAnim, hamburgerCurveAnim } from "./animations";
 import { pageConfig } from "@/utils/pages";
 import styles from "./styles.module.scss";
 import Link from "next/link";
@@ -15,6 +14,48 @@ function HamburgerMenuComponent() {
   const menuOpen = usePageStore((state) => state.menuOpen);
   const toggleMenu = usePageStore((state) => state.toggleMenu);
   const checkActivePage = useActivePath();
+  const initialCurvePath = `M0 0 L${window.innerWidth} 0 Q ${window.innerWidth / 2} 200 0 0`;
+  const targetCurvePath = `M0 0 L${window.innerWidth} 0 Q ${window.innerWidth / 2} 0 0 0`;
+
+  const hamburgerMenuAnim = {
+    initial: {
+      y: -600,
+    },
+    animate: {
+      y: 0,
+      transition: {
+        duration: 0.7,
+        ease: [0.76, 0, 0.24, 1],
+      },
+    },
+    exit: {
+      y: -600,
+      transition: {
+        duration: 0.7,
+        ease: [0.76, 0, 0.24, 1],
+      },
+    },
+  };
+
+  const hamburgerCurveAnim = {
+    initial: {
+      d: initialCurvePath,
+    },
+    animate: {
+      d: targetCurvePath,
+      transition: {
+        duration: 0.8,
+        ease: [0.76, 0, 0.24, 1],
+      },
+    },
+    exit: {
+      d: initialCurvePath,
+      transition: {
+        duration: 0.8,
+        ease: [0.76, 0, 0.24, 1],
+      },
+    },
+  };
 
   return (
     <AnimatePresence mode="wait">
