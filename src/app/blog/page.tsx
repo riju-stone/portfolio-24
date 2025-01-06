c9632c03f1bb8ba74e160ec841777f98f116cf2c
Arighna Chakraborty <riju23chakra@gmail.com>
2025-01-06 20:37:00 +0530

    added loader elements for blog and shelf pages

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,15 +2,21 @@
 
 import SkewScrollComponent from "@/components/scroll/Scroll";
 import React from "react";
+import styles from "./page.module.scss"
+import { pp_nueue } from "@/utils/fonts";
 
 function BlogsPage() {
-  return (
-    <main>
-      <SkewScrollComponent>
-        <div>BlogsPage</div>
-      </SkewScrollComponent>
-    </main>
-  );
+    return (
+        <main>
+            <SkewScrollComponent>
+                <div className={styles.blogsPageWrapper}>
+                    <div className={`${styles.blogLoaderText} ${pp_nueue.className}`}>
+                        Hold on! Contemplating...
+                    </div>
+                </div>
+            </SkewScrollComponent>
+        </main>
+    );
 }
 
 export default BlogsPage;
--- src/app/shelf/page.tsx
+++ src/app/shelf/page.tsx
@@ -3,12 +3,19 @@
 import SkewScrollComponent from "@/components/scroll/Scroll";
 import React from "react";
 
+import styles from "./page.module.scss"
+import { pp_nueue } from "@/utils/fonts";
+
 function ShelfPage() {
-  return (
-    <SkewScrollComponent>
-      <div>ShelfPage</div>
-    </SkewScrollComponent>
-  );
+    return (
+        <SkewScrollComponent>
+            <div className={styles.blogsPageWrapper}>
+                <div className={`${styles.blogLoaderText} ${pp_nueue.className}`}>
+                    Getting my shit together...
+                </div>
+            </div>
+        </SkewScrollComponent>
+    );
 }
 
 export default ShelfPage;
