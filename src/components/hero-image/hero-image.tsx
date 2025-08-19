d8b9059b0a731956e992d96dc7fe6e4bc25ed419
Arighna Chakraborty <riju23chakra@gmail.com>
2025-08-19 10:05:57 +0530

    improved hero image rendering

--- src/components/hero-image/hero-image.tsx
+++ src/components/hero-image/hero-image.tsx
@@ -4,18 +4,30 @@ import { motion } from 'motion/react';
 import Image from 'next/image';
 import React from 'react'
 
+const heroImageAnimation = {
+  "initial": {
+    height: "0%",
+    scale: 1.12,
+  },
+  "animate": {
+    height: "100%",
+    scale: 1,
+    transition: {
+      height: { duration: 0.75, ease: [0.22, 1, 0.36, 1], delay: 0.85 },
+      scale: { duration: 0.6, ease: [0.22, 1, 0.36, 1], delay: 1 },
+    },
+  }
+}
+
 function HeroImageComponent() {
   return <motion.div
     style={{ height: "100%", width: "100%" }}
-    initial={{ height: "0%" }}
-    animate={{ height: "100%" }}
-    transition={{ duration: 0.75, ease: [0.22, 1, 0.36, 1], delay: 0.75 }}>
-    <motion.img
-      initial={{ scale: 1.12 }}
-      animate={{ scale: 1 }}
-      transition={{ duration: 1, ease: [0.22, 1, 0.36, 1], delay: 0.85 }}
-      src="/images/hero.webp" alt="hero" />
+    variants={heroImageAnimation}
+    initial="initial"
+    animate="animate"
+  >
+    <Image src="/images/hero.webp" alt="hero" height={500} width={500} quality={80} />
   </motion.div>
 }
 
-export default HeroImageComponent
+export default HeroImageComponent;
