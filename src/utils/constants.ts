0140f711415ffb0ba7be19303967bc71d1d702d1
Arighna Chakraborty <riju23chakra@gmail.com>
2025-10-02 10:14:07 +0530

    chore: minor update to experience content

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,77 +1,76 @@
-export const HEADING_TEXT = `Creative & Full — Stack Engineer`
+export const HEADING_TEXT = `Creative & Full — Stack Engineer`;
 
-export const INTRO_PHRASE = `A full-stack engineer, trying to find a place in the world of instructions and logic; driven by this one simple philosophy — a few lines of code, simplest version of an idea, and the ability to make something off it, and you can make a dent in the universe. Based out of India, I am passionate about solving problems that makes the world more simple and memorable to experience. I strive to make each and every part of the system seamless and a joy to use — whether its the shining armour on the outside or the numerous nuts and bolts keeping it together underneath. To sum up — a guy with the mind of an engineer, heart of an artist.`
+export const INTRO_PHRASE = `A full-stack engineer, trying to find a place in the world of instructions and logic; driven by this one simple philosophy — a few lines of code, simplest version of an idea, and the ability to make something off it, and you can make a dent in the universe. Based out of India, I am passionate about solving problems that makes the world more simple and memorable to experience. I strive to make each and every part of the system seamless and a joy to use — whether its the shining armour on the outside or the numerous nuts and bolts keeping it together underneath. To sum up — a guy with the mind of an engineer, heart of an artist.`;
 
-export const PROJECT_PHRASE = `"Don't make something unless its both necessary and useful; but if it is both necessary and useful; don't hesitate to make it beautiful." — The Shaker Design Philosophy`
+export const PROJECT_PHRASE = `"Don't make something unless its both necessary and useful; but if it is both necessary and useful; don't hesitate to make it beautiful." — The Shaker Design Philosophy`;
 
 export const PROJECT_METADATA = {
-    col1: "name",
-    col2: "stack",
-    col3: "desc",
-    col4: "link",
-}
+  col1: "name",
+  col2: "stack",
+  col3: "desc",
+  col4: "link",
+};
 
 export const PROJECT_DATA = [
-    {
-        _id: "chess",
-        name: "Chess",
-        link: "https://www.github.com/riju-stone/chess",
-        desc: `A simple cross-platform desktop chess app with chess engine written from scratch in javascript using the minimax algorithm.`,
-        stack: "javascript/electron"
-    },
-    {
-        _id: "caligator",
-        name: "Caligator",
-        link: "https://www.github.com/riju-stone/caligator",
-        desc: `A simple yet smart calculator, that understands your language. Capable of evaluating complex expressions and also performing a variety of conversions. Inspired from Numi.`,
-        stack: "javascript/vite/electron"
-    },
-    {
-        _id: "rss",
-        name: "RSS",
-        link: "https://www.github.com/riju-stone/go-rss",
-        desc: `A simple RSS aggregator API capable of fetching and displaying RSS feeds from a variety of sources. Supports key based authentication and rate limiting.`,
-        stack: "go/postgresql"
-
-    },
-    {
-        _id: "former",
-        name: "Former",
-        link: "https://www.github.com/riju-stone/former",
-        desc: `A modular form builder with an in-built drag and drop editor. Inspired from the likes of Google Forms and Typeform.`,
-        stack: "next/typescript/postgresql"
-    },
-]
+  {
+    _id: "chess",
+    name: "Chess",
+    link: "https://www.github.com/riju-stone/chess",
+    desc: `A simple cross-platform desktop chess app with chess engine written from scratch in javascript using the minimax algorithm.`,
+    stack: "javascript/electron",
+  },
+  {
+    _id: "caligator",
+    name: "Caligator",
+    link: "https://www.github.com/riju-stone/caligator",
+    desc: `A simple yet smart calculator, that understands your language. Capable of evaluating complex expressions and also performing a variety of conversions. Inspired from Numi.`,
+    stack: "javascript/vite/electron",
+  },
+  {
+    _id: "rss",
+    name: "RSS",
+    link: "https://www.github.com/riju-stone/go-rss",
+    desc: `A simple RSS aggregator API capable of fetching and displaying RSS feeds from a variety of sources. Supports key based authentication and rate limiting.`,
+    stack: "go/postgresql",
+  },
+  {
+    _id: "former",
+    name: "Former",
+    link: "https://www.github.com/riju-stone/former",
+    desc: `A modular form builder with an in-built drag and drop editor. Inspired from the likes of Google Forms and Typeform.`,
+    stack: "next/typescript/postgresql",
+  },
+];
 
 export const EXPERIENCE_METADATA = {
-    col1: "role",
-    col2: "title",
-    col3: "duration",
-    col4: "link",
-}
+  col1: "role",
+  col2: "title",
+  col3: "duration",
+  col4: "link",
+};
 
-export const EXPERIENCE_PHRASE = `"The best performance improvement is the transition from the nonworking state to the working state." — John Ousterhout`
+export const EXPERIENCE_PHRASE = `"The best performance improvement is the transition from the nonworking state to the working state." — John Ousterhout`;
 
 export const EXPERIENCE_DATA = [
-    {
-        _id: "session-ai",
-        title: "Session AI Inc.",
-        role: "SWE II",
-        duration: "2022 - Present",
-        link: "https://www.sessionai.com"
-    },
-    {
-        _id: "simulacra-tech",
-        title: "Simulacra Technologies",
-        role: "Freelance",
-        duration: "2021 - 2022",
-        link: "https://www.linkedin.com/company/simulacra-technologies/"
-    },
-    {
-        _id: "skill-academia",
-        title: "Skill Academia",
-        role: "SWE Intern",
-        duration: "2021",
-        link: "https://www.skillacademia.com"
-    }
-]
+  {
+    _id: "session-ai",
+    title: "Session AI",
+    role: "SWE II",
+    duration: "2022 - Present",
+    link: "https://www.sessionai.com",
+  },
+  {
+    _id: "simulacra-tech",
+    title: "Simulacra Technologies",
+    role: "Freelance",
+    duration: "2021 - 2022",
+    link: "https://www.linkedin.com/company/simulacra-technologies/",
+  },
+  {
+    _id: "skill-academia",
+    title: "Skill Academia",
+    role: "SWE Intern",
+    duration: "2021",
+    link: "https://www.skillacademia.com",
+  },
+];
